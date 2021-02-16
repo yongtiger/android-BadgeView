d3922ec20d8df2b88894fa6476fabb2c0688f0b4
Robert Han <brainbook.cc@outlook.com>
2021-02-16 03:56:26 -0800

    Initial commit

---
+++ app/src/main/java/cc/brainbook/android/badgeview/app/ListViewActivity.java
@@ -0,0 +1,83 @@
+package cc.brainbook.android.badgeview.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import cc.brainbook.android.badgeview.Badge;
+import cc.brainbook.android.badgeview.QBadgeView;
+
+public class ListViewActivity extends AppCompatActivity {
+    ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(new ListAdapter());
+    }
+
+    class ListAdapter extends BaseAdapter {
+        private List<String> data;
+
+        public ListAdapter() {
+            data = new DataSupport().getData();
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            Holder holder;
+            if (convertView == null) {
+                holder = new Holder();
+                convertView = LayoutInflater.from(ListViewActivity.this).inflate(R.layout.item_view, parent, false);
+                holder.textView = (TextView) convertView.findViewById(R.id.tv_content);
+                holder.badge = new QBadgeView(ListViewActivity.this).bindTarget(convertView.findViewById(R.id.imageview));
+                holder.badge.setBadgeTextSize(12, true);
+                convertView.setTag(holder);
+            } else {
+                holder = (Holder) convertView.getTag();
+            }
+            holder.textView.setText(data.get(position));
+            holder.badge.setBadgeNumber(position);
+            holder.badge.setOnDragStateChangedListener(new Badge.OnDragStateChangedListener() {
+                @Override
+                public void onDragStateChanged(int dragState, Badge badge, View targetView) {
+                    if (dragState == STATE_SUCCEED) {
+                        Toast.makeText(ListViewActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            return convertView;
+        }
+
+        class Holder {
+            TextView textView;
+            Badge badge;
+        }
+    }
+}
